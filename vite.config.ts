fe55add4b1c2db09f1ae49f9bcc2675c6f442724
tachyondev <patilrani005@gmail.com>
2025-12-27 03:16:02 +0530

    Removed unused code

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
   return {
     server: {
       port: 3000,
@@ -11,10 +10,6 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     base: '/',
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
@@ -25,7 +20,8 @@ export default defineConfig(({ mode }) => {
         output: {
           manualChunks: {
             'vendor-three': ['three', '@react-three/fiber', '@react-three/drei'],
-            'vendor-react': ['react', 'react-dom']
+            'vendor-react': ['react', 'react-dom'],
+            'vendor-framer': ['framer-motion']
           }
         }
       }
